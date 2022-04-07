a4321f6d2d26fcc872a27fa5a85503ffd81d3995
Gaurav Kumar <gauravkr2179@gmail.com>
2022-04-07 22:31:04 +0530

    update

---
+++ assignment/src/components/List.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+function List(){
+    return(
+        <>
+        <div>
+            <h1>Mobile Operating System</h1>
+            <ul>
+                <li>Android</li>
+                <li>Blackberry</li>
+                <li>iPhone</li>
+                <li>Windows Phone</li>
+            </ul>
+        </div>
+        <div>
+            <h1>Mobile Manufactures</h1>
+            <ul>
+                <li>Samsang</li>
+                <li>HTC</li>
+                <li>Micromax</li>
+                <li>Apple</li>
+            </ul>
+        </div>
+        </>
+    )
+}
+
+export default List;
